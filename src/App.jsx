833747808acf3d92388dc374e78d358ef58c7603
alessandro-001 <alessandrofrondini@gmail.com>
2023-09-08 09:41:21 +0100

    connected profile page to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar'
 import Recipes from './components/Recipes'
 import Home from './components/Home'
 import AboutMe from './components/AboutMe'
+import Profile from './components/Profile'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 
@@ -17,6 +18,7 @@ function App() {
           <Route path='/' element={<Home/>} />
           <Route path='/recipes' element={<Recipes/>} />
           <Route path='/about' element={<AboutMe/>} />
+          <Route path='/profile' element={<Profile/>} />
         </Routes>
       </>
     </BrowserRouter>
