c986ae717fdd4ef7c2ec6bd862c13b5de8922a77
alessandro-001 <alessandrofrondini@gmail.com>
2023-09-11 10:45:35 +0100

    fixes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,40 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+      <h1>Welcome to Cucina Cazzereccia</h1>
+      <br />
+      <div className='text-sm mx-5'>
+        <p>
+          My very own recipe haven, where I take the art of cooking to a new level of fun and experimentation! 🍳
+          Are you ready to join me on this culinary adventure filled with unique flavors, my creative twists, and a whole lot of 
+          delicious fun? If you're tired of the same old recipes and crave some excitement in the kitchen, you've landed in the perfect spot.
+        </p>
+        <br />
+        <p>
+          Here at my little corner of the culinary world, I celebrate the joy of cooking as a personal and creative expression. 
+          These recipes are not about perfection; they're about exploring new tastes, trying out inventive variations, and, above all, 
+          having a blast while I'm at it.
+        </p>
+        <br />
+        <p>
+          My recipes are all about personal flair and the thrill of crafting mouthwatering dishes that reflect my unique style and taste. 
+          I firmly believe that every kitchen is a stage, and you're the star of your own show.
+        </p>
+        <br />
+        <p>
+          Join me by creating your account today, and unlock the ability to save and revisit your favorite recipes. 
+          Whether you're a seasoned cook or just starting out, 
+          I welcome you to my kitchen, where we'll explore the endless possibilities of cooking, one dish at a time.
+        </p>
+        <br />
+        <p>
+          Happy cooking! 🍽️👩‍🍳👨‍🍳
+        </p>
+      </div>
+      <div className='mt-5'>
+        <button className='px-3 py-1 rounded-full border border-slate-900 bg-slate-800 text-white'>Sign up!</button>
+      </div>
+    </>
   )
 }
