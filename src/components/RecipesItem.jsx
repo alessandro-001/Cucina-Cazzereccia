77000899675a28405910d4dc894b0746d3102ed5
alessandro-001 <alessandrofrondini@gmail.com>
2023-09-12 13:50:27 +0100

    recipe

---
+++ src/components/RecipesItem.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function RecipesItem() {
+  return (
+    <div>RecipesItem</div>
+  )
+}
---
+++ src/data/recipesList.js
@@ -0,0 +1,9 @@
+const recipesList = [
+    {
+        title: 'Marinated Cougettes',
+        imgUrl: '',
+        description: 'recipe steps here',
+    }
+]
+
+export default recipesList;
